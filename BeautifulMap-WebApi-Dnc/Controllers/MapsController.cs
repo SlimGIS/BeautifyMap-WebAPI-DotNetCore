ae74194841d25d343a6f8b022425d80b972dc22f
SlimGIS_dev <dev@slimgis.com>
2017-05-08 01:20:39 +0800

    Init project.

---
+++ BeautifulMap-WebApi-Dnc/Controllers/MapsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlimGis.MapKit.Geometries;
+using SlimGis.MapKit.Layers;
+using SlimGis.MapKit.Symbologies;
+using SlimGis.MapKit.WebApi;
+
+namespace BeautifulMap_WebApi_Dnc.Controllers
+{
+    [Route("api/[controller]")]
+    public class MapsController : Controller
+    {
+        private static MapModelManager mapModelManager = new MapModelManager();
+
+        // GET api/values
+        [HttpGet("{theme}/{z}/{x}/{y}")]
+        public IActionResult Get(string theme, int z, int x, int y)
+        {
+            //MapModel mapModel = mapModelManager.Dequeue(theme, () =>
+            //{
+            //    string themeFilename = char.ToUpper(theme[0]) + theme.Substring(1);
+            //    CssDocument cssDoc = CssDocument.Load($@"Themes\{themeFilename}Theme.sgcss");
+            //    LayerGroup layerGroup = new LayerGroup();
+            //    layerGroup.LoadStyledLayers("AppData", cssDoc);
+
+            //    MapModel tempMapModel = new MapModel(GeoUnit.Meter);
+            //    tempMapModel.Layers.AddRange(layerGroup.Layers);
+            //    return tempMapModel;
+            //});
+            //return new XyzTileResult(mapModel, x, y, z);
+
+            string themeFilename = char.ToUpper(theme[0]) + theme.Substring(1);
+            CssDocument cssDoc = CssDocument.Load($@"Themes\{themeFilename}Theme.sgcss");
+            LayerGroup layerGroup = new LayerGroup();
+            layerGroup.LoadStyledLayers("AppData", cssDoc);
+
+            MapModel tempMapModel = new MapModel(GeoUnit.Meter);
+            tempMapModel.Layers.AddRange(layerGroup.Layers);
+            return new XyzTileResult(tempMapModel, x, y, z);
+        }
+
+        [HttpGet("{theme}/css")]
+        public IActionResult Get(string theme)
+        {
+            string themeFilename = char.ToUpper(theme[0]) + theme.Substring(1);
+            themeFilename = $@"Themes\{themeFilename}Theme.sgcss";
+
+            if (!System.IO.File.Exists(themeFilename)) return NotFound(theme);
+            return this.Content(System.IO.File.ReadAllText(themeFilename));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Beautify map - MapKit for SlimGIS MapKit WebApi with ASP.NET Core
+In the [previous thread](https://github.com/SlimGIS/Quickstart-WebAPI-DotNetCore), we guided you to walk through a simple quick start sample of SlimGIS MapKit for WinForms. In this thread, we will show you how to build a beautify map. Here is a preview of our samples.
+
+![webapi-dnc-preview-view](https://github.com/SlimGIS/BeautifyMap-WebAPI-DotNetCore/blob/master/Screenshots/Screenshot-Preview.png)
+
+In the basic sample, we will show you 3 kinds of theme.
+1. Dark
+
+![webapi-dnc-dark-view](https://github.com/SlimGIS/BeautifyMap-WebAPI-DotNetCore/blob/master/Screenshots/Screenshot-Dark.png)
+
+2. Light
+
+![webapi-dnc-light-view](https://github.com/SlimGIS/BeautifyMap-WebAPI-DotNetCore/blob/master/Screenshots/Screenshot-Light.png)
+
+3. Blue
+
+![webapi-dnc-blue-view](https://github.com/SlimGIS/BeautifyMap-WebAPI-DotNetCore/blob/master/Screenshots/Screenshot-Blue.png)
